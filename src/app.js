99aba9f85a121bcd3570a89412e05a5ea59a488f
KevinAndSH <kevin@uninorte.edu.co>
2021-10-20 14:56:37 -0500

    cambios finales, creo

--- src/app.js
+++ src/app.js
@@ -15,4 +15,21 @@ app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/register.html"))
 })
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+
+app.post("/search", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.post("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.post("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
 app.listen(port, () => console.log("Server running in port", port))
