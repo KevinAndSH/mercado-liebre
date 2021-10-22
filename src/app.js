99ba16f6142625262275d0d951bcb75371a2bd91
KevinAndSH <kevin@uninorte.edu.co>
2021-10-22 08:31:26 -0500

    arreglos en el ancho de los formularios

--- src/app.js
+++ src/app.js
@@ -3,33 +3,13 @@ const app = express()
 
 const path = require("path")
 
+const rutas = require("./routes/home.routes")
+
 let port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "./public")))
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"))
-})
-
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/register.html"))
-})
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"))
-})
+app.use("/", rutas)
 
 
-app.post("/search", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"))
-})
-
-app.post("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"))
-})
-
-app.post("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"))
-})
 
 app.listen(port, () => console.log("Server running in port", port))
