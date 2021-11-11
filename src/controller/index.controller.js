0f4cda1c77949419d146f0d36a9319fac9aa66ff
KevinAndSH <kevin@uninorte.edu.co>
2021-11-10 22:06:29 -0500

    implementación de EJS en la página principal

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -2,10 +2,13 @@ const express = require("express")
 const app = express()
 const path = require("path")
 
+const basedOn = require("../model/based-on.json")
+const onSale = require("../model/on-sale.json")
+
 
 const controller = {
   home: (req, res) => {
-    res.render("home")
+    res.render("home", {basedOn, onSale})
   },
 
   register: (req, res) => {
